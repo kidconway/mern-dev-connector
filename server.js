90faf3ae81d67b75acab709a393f434915dfbf29
Colby Horan <colbyhoran@gmail.com>
2020-05-27 14:55:45 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3791;
+
+app.get('/', (req, res) => res.send('API Running'));
+
+app.listen(PORT, () => console.log(`One server to rule them all running on port ${PORT}`));
