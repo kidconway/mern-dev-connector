59b3a371bda3e99084359207376ba6afba1c4a6f
Colby Horan <colbyhoran@gmail.com>
2020-05-28 10:59:16 -0500

    minor tweaks

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-//@route    GET api/post
+//@route    GET api/posts
 //@desc     Test route
 //@access   Public
 router.get('/', (req, res) => res.send('Posts route'));
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const connectDB = require('./config/db')
+const express = require("express");
+const connectDB = require("./config/db");
 
 // Connect Database
-connectDB()
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 3791;
 
-app.get('/', (req, res) => res.send('API Running'));
+app.get("/", (req, res) => res.send("API Running"));
 
 // Define routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
-app.listen(PORT, () => console.log(`One server to rule them all running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`One server to rule them all running on port ${PORT}`)
+);
