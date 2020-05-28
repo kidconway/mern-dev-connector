b81030c99de23d8c917dbd5e3c0fd2a673722092
Colby Horan <colbyhoran@gmail.com>
2020-05-28 10:43:05 -0500

    connected routes

--- server.js
+++ server.js
@@ -9,4 +9,10 @@ const PORT = process.env.PORT || 3791;
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 app.listen(PORT, () => console.log(`One server to rule them all running on port ${PORT}`));
