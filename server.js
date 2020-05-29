d22d753567a092d97b990cf68199ee0937da3cb4
Colby Horan <colbyhoran@gmail.com>
2020-05-29 14:57:14 -0500

    added find one and update route

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth-token"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.get("/", (req, res) => res.send("API Running"));
 
 // Define routes
